624e98bed525b4a0efba9f5f0954778ad4b77602
TEACHER <azamovhud@gmail.com>
2022-05-14 13:41:21 +0500

    Random Java tutorial

---
+++ src/com/company/Do.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Do {
+    public static void main(String[] args) {
+
+
+        int i = 10;
+        int w = 0;
+        int res = 0;
+
+        do {
+
+            System.out.println(i);
+            i--;
+        } while (i >= 0);
+
+
+
+
+
+    }
+}
